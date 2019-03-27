72ce78da888519c6c3c354016518c208502428c1
Jiameng <jmfan@bu.edu>
2019-03-27 10:39:15 -0400

    range of quadrotor

--- test_a.cpp
+++ test_a.cpp
@@ -14,8 +14,8 @@ int main(void)
 	char const *function_name1 = "poly_approx_controller";
 	char const *function_name2 = "poly_approx_error";
 	char const *function_name3 = "network_lips";
-	char const *degree_bound = "[3, 3]";
-	char const *box = "[[ 0, 0.01 ],[ 0, 0.01 ]]";
+	char const *degree_bound = "[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]";
+	char const *box = "[[-1, 1],[-1, 1], [9, 10], [-1, 1], [-1, 1], [-1, 1], [0, 0.001], [0, 0.001], [0, 0.001], [1, 1.001], [-1, 1], [-1, 1], [-1, 1], [0, 0.001], [0, 0.001], [0, 0.001], [0, 0.001], [0, 0.001]]";
 	char const *lips = "71.9486";
 	char const *activation = "ReLU";
 	char const *output_index = "0";
