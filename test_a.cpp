4bbf94353687766129c8f5459bcdb4fa1bb4f513
Jiameng <jmfan@bu.edu>
2019-03-27 10:52:22 -0400

    exp 19

--- test_a.cpp
+++ test_a.cpp
@@ -14,12 +14,12 @@ int main(void)
 	char const *function_name1 = "poly_approx_controller";
 	char const *function_name2 = "poly_approx_error";
 	char const *function_name3 = "network_lips";
-	char const *degree_bound = "[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]";
-	char const *box = "[[-1, 1],[-1, 1], [9, 10], [-1, 1], [-1, 1], [-1, 1], [0, 0.001], [0, 0.001], [0, 0.001], [1, 1.001], [-1, 1], [-1, 1], [-1, 1], [0, 0.001], [0, 0.001], [0, 0.001], [0, 0.001], [0, 0.001]]";
+	char const *degree_bound = "[3, 3, 3, 3]";
+	char const *box = "[[0.5, 1.5],[0.5, 1.5], [0.5, 1.5], [0.5, 1.5]]";
 	char const *lips = "71.9486";
 	char const *activation = "ReLU";
 	char const *output_index = "0";
-	char const *neural_network = "nn_controller_quadrotor";
+	char const *neural_network = "nn_19_relu";
 	cout << "Result of call polynomial generation function: " << bernsteinPolyApproximation(module_name, function_name1, degree_bound, box, activation, output_index, neural_network) << endl;
 	//string new_lips = bernsteinPolyApproximation(module_name, function_name3, degree_bound, box, activation, output_index);
 	//cout << "Result of estimating Lipschitz constant: " << new_lips << endl;
