a23035bcbfaf203e10b18d80a7013d795422bc1d
ChaoHuang2018 <thomashuangchao@gmail.com>
2019-04-11 01:52:56 -0500

    exmaples

--- nn_18_sigmoid.cpp
+++ nn_18_sigmoid.cpp
@@ -1,5 +1,7 @@
 #include "../flowstar/Continuous.h"
-#include "../Bernstein_Polynomial_Approximation/bernstein_poly_approx.h"
+#include "bernstein_poly_approx.h"
+#include<fstream>
+#include<ctime>
 
 using namespace std;
 using namespace flowstar;
@@ -8,41 +10,44 @@ using namespace flowstar;
 int main()
 {
 	// Declaration of the state variables.
-	unsigned int numVars = 3;
+	unsigned int numVars = 4;
 
 	int x0_id = stateVars.declareVar("x0");
 	int x1_id = stateVars.declareVar("x1");
+	int x2_id = stateVars.declareVar("x2");
 	int u_id = stateVars.declareVar("u");
 
 	int domainDim = numVars + 1;
 
 
 	// Define the continuous dynamics.
-	Expression_AST<Real> deriv_x0("x1 - x0^3");  // theta_r = 0
-	Expression_AST<Real> deriv_x1("u");
+	Expression_AST<Real> deriv_x0("x2^3-x1");  // theta_r = 0
+	Expression_AST<Real> deriv_x1("x2");
+	Expression_AST<Real> deriv_x2("u");
 	Expression_AST<Real> deriv_u("0");
 
 	vector<Expression_AST<Real> > ode_rhs(numVars);
 	ode_rhs[x0_id] = deriv_x0;
 	ode_rhs[x1_id] = deriv_x1;
+	ode_rhs[x2_id] = deriv_x2;
 	ode_rhs[u_id] = deriv_u;
 
 	Deterministic_Continuous_Dynamics dynamics(ode_rhs);
 
 
 
-	
+
 	// Specify the parameters for reachability computation.
-	
+
 	Computational_Setting setting;
 
-	unsigned int order = 5;
+	unsigned int order = 12;
 
 	// stepsize and order for reachability analysis
 	setting.setFixedStepsize(0.02, order);
 
 	// time horizon for a single control step
-	setting.setTime(0.2);
+	setting.setTime(0.5);
 
 	// cutoff threshold
 	setting.setCutoffThreshold(1e-10);
@@ -58,7 +63,7 @@ int main()
 	vector<Interval> remainder_estimation(numVars, I);
 	setting.setRemainderEstimation(remainder_estimation);
 
-setting.printOff();
+	setting.printOff();
 
 	setting.prepare();
 
@@ -66,10 +71,11 @@ setting.printOff();
 	 * Initial set can be a box which is represented by a vector of intervals.
 	 * The i-th component denotes the initial set of the i-th state variable.
 	 */
-	Interval init_x0(0.7,0.9), init_t_err(0.7,0.9), init_u(0);
+	Interval init_x0(0.35, 0.45), init_x1(0.45, 0.55), init_x2(0.25, 0.35), init_u(0);
 	std::vector<Interval> X0;
 	X0.push_back(init_x0);
-	X0.push_back(init_t_err);
+	X0.push_back(init_x1);
+	X0.push_back(init_x2);
 	X0.push_back(init_u);
 
 
@@ -89,48 +95,62 @@ setting.printOff();
 	char const *function_name1 = "poly_approx_controller";
 	char const *function_name2 = "poly_approx_error";
 	char const *function_name3 = "network_lips";
-	char const *degree_bound = "[1, 1]";
-//	char const *activation = "ReLU";
-	char const *activation = "tanh";
+	char const *degree_bound = "[2, 2, 2]";
+	// char const *activation = "ReLU";
+	char const *activation = "sigmoid";
+	// char const *activation = "tanh";
 	char const *output_index = "0";
-	char const *neural_network = "neural_network_controller_tanh";
-	
-//	double pi = 3.14159;
-//	double factor = 2*pi;
+	char const *neural_network = "nn_18_sigmoid";
 
-	// perform 20 control steps
-	for(int iter=0; iter<20; ++iter)
+	//	double pi = 3.14159;
+	//	double factor = 2*pi;
+
+	double err_max = 0;
+	time_t start_timer;
+	time_t end_timer;
+	double seconds;
+	time(&start_timer);
+
+	// perform 30 control steps
+	for (int iter = 0; iter < 10; ++iter)
 	{
+		cout << "step: " << iter << endl;
+
 		vector<Interval> box;
 		initial_set.intEval(box, order, setting.tm_setting.cutoff_threshold);
-/*		
-		if(box[1].sup() > 0)
-		{
-			for(; box[1].sup() > pi;)
-			{
-				box[1].setSup(box[1].sup() - factor);
-				box[1].setInf(box[1].inf() - factor);
-			}
-		}
-		else
-		{
-			for(; box[1].inf() < -pi;)
-			{
-				box[1].setSup(box[1].sup() + factor);
-				box[1].setInf(box[1].inf() + factor);
-			}
-		}
-*/		
+		/*
+				if(box[1].sup() > 0)
+				{
+					for(; box[1].sup() > pi;)
+					{
+						box[1].setSup(box[1].sup() - factor);
+						box[1].setInf(box[1].inf() - factor);
+					}
+				}
+				else
+				{
+					for(; box[1].inf() < -pi;)
+					{
+						box[1].setSup(box[1].sup() + factor);
+						box[1].setInf(box[1].inf() + factor);
+					}
+				}
+		*/
 
-		string strBox = "[" + box[0].toString() + "," + box[1].toString() + "]";
-//cout << strBox <<endl;
+		string strBox = "[" + box[0].toString() + "," + box[1].toString() + "," + box[2].toString() + "]";
+		//cout << strBox <<endl;
 
 		string strExpU = bernsteinPolyApproximation(module_name, function_name1, degree_bound, strBox.c_str(), activation, output_index, neural_network);
-		
-		
+
+
 		double err = stod(bernsteinPolyApproximation(module_name, function_name2, degree_bound, strBox.c_str(), activation, output_index, neural_network));
 
-printf("%e\n", err);
+		if (err >= err_max)
+		{
+			err_max = err;
+		}
+
+		printf("%e\n", err);
 		Expression_AST<Real> exp_u(strExpU);
 
 		TaylorModel<Real> tm_u;
@@ -140,25 +160,25 @@ printf("%e\n", err);
 
 		tm_u.remainder.bloat(err);
 
-//Interval range_of_flowpipe;
-//tm_u.intEvalNormal(range_of_flowpipe, setting.tm_setting.step_exp_table);
+		//Interval range_of_flowpipe;
+		//tm_u.intEvalNormal(range_of_flowpipe, setting.tm_setting.step_exp_table);
 
-//cout << range_of_flowpipe << "\n";
+		//cout << range_of_flowpipe << "\n";
 
-		
-	
+
+
 		initial_set.tmvPre.tms[u_id] = tm_u;
-		
-/*		
-TaylorModelVec<Real> tmvTemp;
-initial_set.compose(tmvTemp, order, setting.tm_setting.cutoff_threshold);
-tmvTemp.tms[u_id].intEval(range_of_flowpipe, initial_set.domain);
 
-cout << range_of_flowpipe << "\n";
-*/
+		/*
+		TaylorModelVec<Real> tmvTemp;
+		initial_set.compose(tmvTemp, order, setting.tm_setting.cutoff_threshold);
+		tmvTemp.tms[u_id].intEval(range_of_flowpipe, initial_set.domain);
+
+		cout << range_of_flowpipe << "\n";
+		*/
 		dynamics.reach(result, setting, initial_set, unsafeSet);
 
-		if(result.status == COMPLETED_SAFE || result.status == COMPLETED_UNSAFE || result.status == COMPLETED_UNKNOWN)
+		if (result.status == COMPLETED_SAFE || result.status == COMPLETED_UNSAFE || result.status == COMPLETED_UNKNOWN)
 		{
 			initial_set = result.fp_end_of_time;
 		}
@@ -168,6 +188,8 @@ cout << range_of_flowpipe << "\n";
 		}
 	}
 
+	time(&end_timer);
+	seconds = difftime(start_timer, end_timer);
 
 	// plot the flowpipes in the x-y plane
 	result.transformToTaylorModels(setting);
@@ -176,15 +198,21 @@ cout << range_of_flowpipe << "\n";
 	plot_setting.setOutputDims(x0_id, x1_id);
 
 	int mkres = mkdir("./outputs", S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
-	if(mkres < 0 && errno != EEXIST)
+	if (mkres < 0 && errno != EEXIST)
 	{
 		printf("Can not create the directory for images.\n");
 		exit(1);
 	}
 
+	ofstream result_output("./outputs/nn_18_sigmoid.txt");
+	if (result_output.is_open())
+	{
+		result_output << err_max << endl;
+		result_output << seconds << endl;
+	}
 	// you need to create a subdir named outputs
 	// the file name is example.m and it is put in the subdir outputs
-	plot_setting.plot_2D_interval_MATLAB("example", result);
+	plot_setting.plot_2D_interval_MATLAB("nn_18_sigmoid", result);
 
 	return 0;
 }
