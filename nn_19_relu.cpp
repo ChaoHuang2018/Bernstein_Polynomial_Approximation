620767f2bbe107cd19857da02e7ecebbd102062b
Jiameng <jmfan@bu.edu>
2019-04-11 13:13:56 -0400

    19 relu code

--- nn_19_relu.cpp
+++ nn_19_relu.cpp
@@ -56,7 +56,7 @@ int main()
 	setting.setCutoffThreshold(1e-10);
 
 	// queue size for the symbolic remainder
-	setting.setQueueSize(1000);
+	setting.setQueueSize(10000);
 
 	// print out the steps
 	setting.printOn();
