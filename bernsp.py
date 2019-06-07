fix numerical issue to float64

bernsp.py
@@ -1,7 +1,6 @@
 from scipy.special import comb
 from sympy import *
 from numpy import linalg as LA
-from numpy import pi, tanh, array, dot
 from scipy.optimize import linprog
 
 import numpy as np
@@ -12,32 +11,34 @@ import math
 
 def nn_poly_approx_bernstein(f, state_vars, d, box, output_index):
     """
-    bernstein polynomial approximation of a given function f on a general box space
+    bernstein polynomial approximation of a given function f
+    on a general box space
     f: a function
     state_var: the input variable of f
     d: degree bound vector of bernstein polynomial
-    box: box space of state variables [\alpha_1,\beta_1]\times \cdots \times [\alpha_m,\beta_m]
+    box: box space of state variables [alpha_1, beta_1] times cdots
+    times [alpha_m, beta_m]
     """
     m = len(state_vars)
     x = state_vars
-    all_comb_lists = degree_comb_lists(d,m)
+    all_comb_lists = degree_comb_lists(d, m)
     bernstein = 0
-    poly_min = math.inf
-    poly_max = -math.inf
+    poly_min = np.inf
+    poly_max = -np.inf
     # construct bernstein polynomial for recover function + nerual network
     y = sp.symbols('y:'+str(m))
     for cb in all_comb_lists:
-        point = [];
+        point = []
         for j in range(m):
             k_j = cb[j]
             d_j = d[j]
             # linear transformation to normalize the box to I=[0,1]^m
             # lower bound of the j-th component
-            alpha_j = float(box[j][0])
+            alpha_j = np.float64(box[j][0])
             # upper bound of the j-th component
-            beta_j = float(box[j][1])
+            beta_j = np.float64(box[j][1])
             point.append((beta_j-alpha_j)*(cb[j]/d[j])+alpha_j)
-        monomial = f(np.array(point))[output_index]
+        monomial = f(np.array(point, dtype=np.float64))[output_index]
         if monomial < poly_min:
             poly_min = monomial
         if monomial > poly_max:
@@ -46,10 +47,8 @@ def nn_poly_approx_bernstein(f, state_vars, d, box, output_index):
             y_j = y[j]
             k_j = cb[j]
             d_j = d[j]
-            monomial = monomial*round(comb(d_j,k_j))*(y_j**k_j)*((1-y_j)**(d_j-k_j))
+            monomial = monomial*round(comb(d_j, k_j))*(y_j**k_j)*((1-y_j)**(d_j-k_j))
         bernstein = bernstein + monomial
-    # print(p2c(bernstein))
-    # construct polynomial approximation for the overall controller based on bernstein polynomial
     poly_approx = bernstein[0]
     for j in range(m):
         y_j = y[j]
@@ -59,10 +58,12 @@ def nn_poly_approx_bernstein(f, state_vars, d, box, output_index):
         poly_approx = poly_approx.subs(y_j, (x_j-alpha_j)/(beta_j-alpha_j))
     return simplify(poly_approx), poly_min[0], poly_max[0]
 
+
 def bernstein_error(f_details, f, d, box, output_index, activation, filename):
     lips, network_output_range = lipschitz(f_details, box, output_index, activation)
     if isinstance(lips, np.ndarray):
         lips = lips[0]
+    print('---------------' + filename + '-------------------')
     print('Lipschitz constant: {}'.format(lips))
 
     m = len(d)
@@ -78,12 +79,14 @@ def bernstein_error(f_details, f, d, box, output_index, activation, filename):
         error_bound_lips = error_bound_lips * (beta_j-alpha_j)
     error_bound_lips = error_bound_lips * math.sqrt(temp)
 
-
-    x = sp.symbols('x:'+ str(f_details.num_of_inputs))
+    x = sp.symbols('x:' + str(f_details.num_of_inputs))
     b, poly_min, poly_max = nn_poly_approx_bernstein(f, x, d, box, output_index)
     error_bound_interval = max([poly_min-network_output_range[0][0][0], network_output_range[0][1][0]-poly_max, 0])
+    if error_bound_interval <= np.finfo(np.float64).eps:
+        error_bound_interval = 0.0
 
-    print('network_output_range: {}'.format(network_output_range[0]))
+    print('network_output_range: {}'.format(np.reshape(network_output_range[0],
+                                                       (1, -1))[0]))
     print('poly_range: {}'.format([poly_min, poly_max]))
     print('error_bound_lips: {}'.format(error_bound_lips))
     print('error_bound_interval: {}'.format(error_bound_interval))
@@ -91,8 +94,12 @@ def bernstein_error(f_details, f, d, box, output_index, activation, filename):
         flag = '0,'
     else:
         flag = '1,'
-    with open('outputs/times/' + filename + 'count.txt', 'a+') as file:
+    with open('outputs/times/' + filename + '_count.txt', 'a+') as file:
         file.write(flag)
+    with open('outputs/errors/' + filename + '_lip_errors.txt', 'a+') as file:
+        file.write("{}\n".format(error_bound_lips))
+    with open('outputs/errors/' + filename + '_interval_errors.txt', 'a+') as file:
+        file.write("{}\n".format(error_bound_interval))
     return min([error_bound_lips, error_bound_interval])
 
 
@@ -105,7 +112,7 @@ def lipschitz(NN_controller, network_input_box, output_index, activation):
 
     layers = len(bias_all_layer)
     lips = 1
-    input_range_layer = network_input_box
+    input_range_layer = np.float64(network_input_box)
     for j in range(layers):
         if j < layers - 1:
             weight_j = weight_all_layer[j]
@@ -118,7 +125,8 @@ def lipschitz(NN_controller, network_input_box, output_index, activation):
         lipschitz_j = lipschitz_layer(weight_j, bias_j, input_range_layer, activation)
         lips = lips * lipschitz_j
         input_range_layer, _ = output_range_layer(weight_j, bias_j, input_range_layer, activation)
-    return lips* scale_factor, (np.array(input_range_layer)-offset)* scale_factor
+    return lips * scale_factor, (np.array(input_range_layer, dtype=np.float64)-offset) * scale_factor
+
 
 def lipschitz_layer(weight, bias, input_range_layer, activation):
     neuron_dim = bias.shape[0]
@@ -134,7 +142,7 @@ def lipschitz_layer(weight, bias, input_range_layer, activation):
             elif range_j[1] < 0.5:
                 singular_j = range_j[1]*(1-range_j[1])
             else:
-                singular_j = np.array([0.25])
+                singular_j = np.array([0.25], dtype=np.float64)
             if max_singular < singular_j:
                 max_singular = singular_j
         return max_singular*LA.norm(weight, 2)
@@ -147,10 +155,11 @@ def lipschitz_layer(weight, bias, input_range_layer, activation):
             elif range_j[1] < 0:
                 singular_j = 1 - range_j[1]**2
             else:
-                singular_j = np.array([1])
+                singular_j = np.array([1.0], dtype=np.float64)
             if max_singular < singular_j:
                 max_singular = singular_j
         return max_singular*LA.norm(weight, 2)
+
 
 def output_range_layer(weight, bias, input_range_layer, activation):
     # solving LPs
@@ -161,17 +170,14 @@ def output_range_layer(weight, bias, input_range_layer, activation):
         # c: weight of the j-th dimension
         c = weight[j]
         c = c.transpose()
-        #print('c: ' + str(c))
-        b = bias[j]
-        #print('b: ' + str(b))
+        b = bias[j][0]
         # compute the minimal input
         res_min = linprog(c, bounds=input_range_layer, options={"disp": False})
-        input_j_min = res_min.fun + b
-        #print('min: ' + str(input_j_min))
+        input_j_min = np.array([res_min.fun + b], dtype=np.float64)
         # compute the minimal output
         if activation == 'ReLU':
             if input_j_min < 0:
-                output_j_min = np.array([0])
+                output_j_min = np.array([0.0], dtype=np.float64)
             else:
                 output_j_min = input_j_min
         if activation == 'sigmoid':
@@ -180,11 +186,11 @@ def output_range_layer(weight, bias, input_range_layer, activation):
             output_j_min = 2/(1+np.exp(-2*input_j_min))-1
         # compute the maximal input
         res_max = linprog(-c, bounds=input_range_layer, options={"disp": False})
-        input_j_max = -res_max.fun + b
+        input_j_max = np.array([-res_max.fun + b], dtype=np.float64)
         # compute the maximal output
         if activation == 'ReLU':
             if input_j_max < 0:
-                output_j_max = np.array([0])
+                output_j_max = np.array([0.0], dtype=np.float64)
             else:
                 output_j_max = input_j_max
                 new_weight.append(weight[j])
@@ -205,6 +211,7 @@ def degree_comb_lists(d, m):
     all_comb_lists = list(itertools.product(*degree_lists))
     return all_comb_lists
 
+
 def p2c(py_b):
     str_b = str(py_b)
     c_b = str_b.replace("**", "^")
@@ -214,6 +221,3 @@ def p2c(py_b):
 # a simple test case
 def test_f(x):
     return math.sin(x[0])+math.cos(x[1])
-
-
-

neuralnetwork.py
@@ -31,7 +31,7 @@ class NN(object):
         self.network_structure[-1] = self.num_of_outputs
 
         # all values from the text file
-        self.param = res
+        self.param = np.float64(res)
 
         # store the weights and bias in two lists
         # self.weights
@@ -62,8 +62,8 @@ class NN(object):
 
         # compute parameters of the input layer
         weight_matrix0 = np.zeros((self.network_structure[0],
-                                   self.num_of_inputs))
-        bias_0 = np.zeros((self.network_structure[0], 1))
+                                   self.num_of_inputs), dtype=np.float64)
+        bias_0 = np.zeros((self.network_structure[0], 1), dtype=np.float64)
 
         for i in range(self.network_structure[0]):
             for j in range(self.num_of_inputs):
@@ -80,8 +80,9 @@ class NN(object):
         # compute the hidden layers paramters
         for i in range(self.num_of_hidden_layers):
             weights = np.zeros((self.network_structure[i + 1],
-                                self.network_structure[i]))
-            bias = np.zeros((self.network_structure[i + 1], 1))
+                                self.network_structure[i]), dtype=np.float64)
+            bias = np.zeros((self.network_structure[i + 1], 1),
+                            dtype=np.float64)
 
             # read the weight matrix
             for j in range(self.network_structure[i + 1]):